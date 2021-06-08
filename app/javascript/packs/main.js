ea73d7be2825cef3410ff03118cb3a9682bb8dbc
Samuel1698 <samuel1698500@gmail.com>
2021-06-08 19:52:02 -0400

    Dry up main.js from anchor logic. Add second comicNav

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -49,33 +49,28 @@ function showSearch() {
 // Comic Navigation
 // =======================================================================
 
-let regex = /^[^#]*/;
-let current_strip  = window.location.toString().match(regex).toString(); 
-let first_strip    = document.querySelector(".fa-angle-double-left").href.toString().match(regex).toString();
-let previous_strip = document.querySelector(".fa-angle-left").href.toString().match(regex).toString();
-let next_strip     = document.querySelector(".fa-angle-right").href.toString().match(regex).toString();
-let last_strip     = document.querySelector(".fa-angle-double-right").href.toString().match(regex).toString();
+let current_strip  = window.location; 
+let first_strip    = document.querySelector(".fa-angle-double-left");
+let previous_strip = document.querySelector(".fa-angle-left");
+let next_strip     = document.querySelector(".fa-angle-right");
+let last_strip     = document.querySelector(".fa-angle-double-right");
 
-if (first_strip == current_strip) {
-  let first_strip = document.querySelector(".fa-angle-double-left");  
+if (first_strip.href == current_strip) {
   first_strip.setAttribute('href', "#");
   first_strip.classList.add("disabled");
   first_strip.querySelector(".visually-hidden").remove();
 }
-if (previous_strip == current_strip) {
-  let previous_strip = document.querySelector(".fa-angle-left");
+if (previous_strip.href == current_strip) {
   previous_strip.setAttribute('href', "#");
   previous_strip.classList.add("disabled");
   previous_strip.querySelector(".visually-hidden").remove();
 }
-if (next_strip == current_strip) {
-  let next_strip = document.querySelector(".fa-angle-right");
+if (next_strip.href == current_strip) {
   next_strip.setAttribute('href', "#");
   next_strip.classList.add("disabled");
   next_strip.querySelector(".visually-hidden").remove();
 }
-if (last_strip == current_strip) {
-  let last_strip = document.querySelector(".fa-angle-double-right");
+if (last_strip.href == current_strip) {
   last_strip.setAttribute('href', "#");
   last_strip.classList.add("disabled");
   last_strip.querySelector(".visually-hidden").remove();
