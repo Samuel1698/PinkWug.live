d9b8bed4fc372123f3da58753f68c4ef4ea4b7fa
Samuel1698 <samuel1698500@gmail.com>
2021-06-13 15:00:19 -0400

    Implement tags on Admin dashboard

--- app/admin/strips.rb
+++ app/admin/strips.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Strip do
 
-  permit_params :title, :image, :transcript, :created_at, :updated_at, strip_tags_attributes: [:id, :strip_id, :tag_id, :_destroy]
+  permit_params :title, :image, :transcript, :created_at, :updated_at, strip_tags_attributes: [:id, :title, :strip_id, :tag_id, :_destroy]
 
   filter :title
   filter :tags
@@ -8,16 +8,16 @@ ActiveAdmin.register Strip do
   filter :updated_at
 
 
-  index download_links: false do    
+  index download_links: false do |f|
 
-    selectable_column
-    column :image do |strip|
+    f.selectable_column
+    f.column :image do |strip|
       image_tag url_for(strip.image.variant(resize_to_limit:[200,10000]))
     end
-    column :title
-    column :tags
-    column :created_at
-    actions
+    f.column :title
+    f.column :tags
+    f.column :created_at
+    f.actions
   end
   
   show do 
@@ -27,23 +27,37 @@ ActiveAdmin.register Strip do
         image_tag url_for(strip.image.variant(resize_to_fit:[700,10000]))
       end
       row :transcript
-      row :tags
+      row :tags do |strip|
+        strip.tags.map { |st| st.title }.join(", ").html_safe
+      end
       row :created_at
       row :updated_at
     end
   end
- 
+
   form do |f|
     f.inputs "Strip Details" do
       f.input :title
       f.input :image, as: :file if !f.object.image.attached?
-      f.input :image, image_preview: :true if f.object.image.attached?
-      f.input :transcript, hint: "Not required. Good for accessibility. Describe every panel of the comic"
-      
-      f.has_many :strip_tags, allow_destroy: true do |s|
-        s.input :tag
+      f.input :image, hint: "If you change image file, it wont show up here yet until you click Update Strip!", image_preview: :true if f.object.image.attached?
+      f.input :transcript, hint: "Not required. Good for accessibility. Describe every panel of the comic", :input_html => { :rows => 10 }
+      # f.input :tags, :input_html => {:multiple => true} do
+      #   f.has_many :strip_tags, as: :select, allow_destroy: true do |s|
+      #     s.input :tag, :collection => Tag.all.map { |tg| ["#{tg.title.capitalize}", tg.id] }
+      #   end
+      # end
+      f.has_many :strip_tags, allow_destroy: true do |t|
+        t.input :tag
       end
-      f.input :created_at, as: :date_select
+      
+     
+      # f.has_many :strip_tags, 
+      #            new_record: 'Add Tag', 
+      #            remove_record: 'Remove', 
+      #            allow_destroy: true, new_record: true do |s|
+      #   s.input :tag, as: :datalist
+      # end
+      f.input :created_at, as: :date_picker, label: "Created in"
     end
     f.actions 
   end
