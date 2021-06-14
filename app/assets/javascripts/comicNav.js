c9b08dc45c870b08c9949eaf98c4b78eddd8de87
Samuel1698 <samuel1698500@gmail.com>
2021-06-13 21:26:33 -0400

    Add About Me page. Split js files

---
+++ app/assets/javascripts/comicNav.js
@@ -0,0 +1,32 @@
+// =======================================================================
+// Comic Navigation
+// =======================================================================
+
+var current_strip  = window.location.pathname; 
+var first_strip    = document.querySelectorAll(".fa-angle-double-left");
+var previous_strip = document.querySelectorAll(".fa-angle-left");
+var next_strip     = document.querySelectorAll(".fa-angle-right");
+var last_strip     = document.querySelectorAll(".fa-angle-double-right");
+
+for (i = 0; i < 2; i++){
+  if (first_strip[i].pathname == current_strip) {
+    first_strip[i].setAttribute('href', "#");
+    first_strip[i].classList.add("disabled");
+    first_strip[i].querySelector(".visually-hidden").remove();
+  }
+  if (previous_strip[i].pathname == current_strip) {
+    previous_strip[i].setAttribute('href', "#");
+    previous_strip[i].classList.add("disabled");
+    previous_strip[i].querySelector(".visually-hidden").remove();
+  }
+  if (next_strip[i].pathname == current_strip) {
+    next_strip[i].setAttribute('href', "#");
+    next_strip[i].classList.add("disabled");
+    next_strip[i].querySelector(".visually-hidden").remove();
+  }
+  if (last_strip[i].pathname == current_strip) {
+    last_strip[i].setAttribute('href', "#");
+    last_strip[i].classList.add("disabled");
+    last_strip[i].querySelector(".visually-hidden").remove();
+  }
+}
---
+++ app/assets/javascripts/whiteSpace.js
@@ -0,0 +1,17 @@
+// =======================================================================
+// White Space
+// =======================================================================
+window.addEventListener('resize', changeWindowSize);
+window.addEventListener('turbolinks:load', changeWindowSize);
+
+function changeWindowSize(){
+  var viewPort      = window.innerHeight;
+  var headerHeight  = document.querySelector(".header").offsetHeight;
+  var contentHeight = document.querySelector(".main").offsetHeight;
+  var footerHeight  = document.querySelector(".footer").offsetHeight;
+  
+  var whiteSpace    = viewPort - (headerHeight + contentHeight + footerHeight + 25);
+  whiteSpace = whiteSpace + "px";
+
+  document.querySelector(".white_space").style.minHeight = whiteSpace;
+}
