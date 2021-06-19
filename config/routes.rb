6a0b4212c4e3d1895f81b91f4b16ff2d3acbe617
Samuel1698 <samuel1698500@gmail.com>
2021-06-19 13:56:36 -0400

    Rename strips -> comics and strips#index -> all in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'about' => 'strips#about'
-  resources :strips
+  resources :all, as: :strips, controller: :strips, only: [:index]
+  resources :comics, as: :strips, controller: :strips, only: [:show]
   root 'strips#latest'
 end
