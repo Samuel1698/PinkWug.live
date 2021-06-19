4559a94d0f9cb30a09c67c42dab188161c448bc1
Samuel1698 <samuel1698500@gmail.com>
2021-06-18 23:17:37 -0400

    Archive toggle with js, css flexbox table now working. Introduce scss mixings, yield(:javascripts), modify Read.me

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,24 @@
+var toggle = document.querySelector(".toggle");
+var line          = document.querySelector(".div");
+var table         = document.querySelector(".table");
+var rows          = document.querySelectorAll(".row");
+
+toggle.addEventListener("click", function(){
+	if (table.classList.contains("gallery")){ //If gallery view is on, toggle off
+	  line.classList.add("line");
+	  toggle.classList.remove("gallery");
+		table.classList.remove("gallery");
+
+		for (i = 0; i < rows.length; i++){
+			rows[i].classList.remove("gallery");
+		}
+	}
+	else {                                   //If gallery view is off, toggle on
+		line.classList.remove("line");
+		table.classList.add("gallery");
+		toggle.classList.add("gallery");
+		for (i = 0; i < rows.length; i++){
+			rows[i].classList.add("gallery");
+		}
+	}
+});
