37b0c250cfb1cd949c3a500363a1d46f25053656
Samuel1698 <samuel1698500@gmail.com>
2021-06-19 16:01:25 -0400

    Toggle gallery view when window < 700px

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,7 +3,9 @@ var line          = document.querySelector(".div");
 var table         = document.querySelector(".table");
 var rows          = document.querySelectorAll(".row");
 
-toggle.addEventListener("click", function(){
+toggle.addEventListener("click", gallery);
+
+function gallery(){
 	if (table.classList.contains("gallery")){ //If gallery view is on, toggle off
 	  line.classList.add("line");
 	  toggle.classList.remove("gallery");
@@ -20,7 +22,7 @@ toggle.addEventListener("click", function(){
 			rows[i].classList.add("gallery");
 		}
 	}
-});
+};
 
 window.addEventListener('resize', removeToggle);
 window.addEventListener('turbolinks:visit', removeToggle);
@@ -30,5 +32,11 @@ function removeToggle(){
 	toggle.classList.remove("hidden");
 	if (window.innerWidth <= 700) {
 		toggle.classList.add("hidden");
+		line.classList.remove("line");
+		table.classList.add("gallery");
+		toggle.classList.add("gallery");
+		for (i = 0; i < rows.length; i++){
+			rows[i].classList.add("gallery");
+		}
 	}
 }
