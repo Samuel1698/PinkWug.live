6dd8f58fe807956fb00c076686a96e8b2a433000
Samuel1698 <samuel1698500@gmail.com>
2021-06-29 13:43:57 -0400

    Close search and menu only in mobile screen sizes

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -15,20 +15,25 @@ var searchForm = document.querySelector(".search_form");
   searchToggle.setAttribute("aria-label", "Open search"); //Label for search button
   // Margin
   main.classList.remove("search_margin", "menu_margin");
+
+
 // Navigation Menu
 navToggle.addEventListener("click", function () {
-  if (navWrapper.classList.contains("active")) {
+  if (navWrapper.classList.contains("active")) { //If menu is active when clicked
     this.setAttribute("aria-expanded", "false");
     this.setAttribute("aria-label", "menu");
     navWrapper.classList.remove("active");
     main.classList.remove("search_margin", "menu_margin");
-  } else {
+  } else { // If menu is inactive when clicked
     navWrapper.classList.add("active");
     main.classList.add("menu_margin");
     this.setAttribute("aria-label", "close menu");
     this.setAttribute("aria-expanded", "true");
-    searchForm.classList.remove("active");
-    searchToggle.classList.remove("active");
+    // Close Search only if sccreen size allows
+    if (window.innerWidth <= 540){
+      searchForm.classList.remove("active");
+      searchToggle.classList.remove("active");
+    }
   }
 });
 
@@ -38,13 +43,14 @@ searchToggle.addEventListener("click", function() {
   searchForm.classList.toggle("active");
   searchToggle.classList.toggle("active");
   main.classList.add("search_margin");
-  // Close Menu
-  navToggle.setAttribute("aria-expanded", "false");
-  navToggle.setAttribute("aria-label", "menu");
-  navWrapper.classList.remove("active");
-  // Remove Menu Margin
-  main.classList.remove("menu_margin");
-
+  // Close Menu only if screen size allows 
+  if (window.innerWidth <= 540){
+    navToggle.setAttribute("aria-expanded", "false");
+    navToggle.setAttribute("aria-label", "menu");
+    navWrapper.classList.remove("active");
+    // Remove Menu Margin
+    main.classList.remove("menu_margin");
+  }
   if (searchToggle.classList.contains("active")) {
     searchToggle.setAttribute("aria-label", "Close search");
   } else {
