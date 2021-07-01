8bacabec7895424179fda624d0e2511dea9849b5
Samuel1698 <samuel1698500@gmail.com>
2021-07-01 12:51:54 -0400

    Fix issue #8 and add test for stopwords in search

--- app/models/strip/filter.rb
+++ app/models/strip/filter.rb
@@ -1,15 +1,18 @@
 class Strip::Filter
 	def filter(scope, query_params)
 		# if query_params[:text].present?
-		# 	scope = scope.where("title LIKE :text OR description LIKE :text OR keywords LIKE :text OR transcript LIKE :text", text: "%#{query_params[:text]}%")
-		# end
-		# scope
-		if query_params[:text].present?
-			list = File.read("#{::Rails.root}/app/models/strip/stop-words.txt").split.join('|')
-			stopWords = /\b(?:#{list})\b/i
-			words = query_params[:text].downcase.strip.gsub(stopWords, '').split.uniq
+		list = File.read("#{::Rails.root}/app/models/strip/stop-words.txt").split.join('|')
+		stopWords = /\b(?:#{list})\b/i
+		words = query_params[:text].downcase.strip.gsub(stopWords, '').split.uniq
+		# If the array is empty after excluding all the stop words
+		if !words.any?
+			scope = scope.where("title LIKE ?", ' ') # Returnn nothing. Must be a better way than doing a query
+		else
 			scope = scope.where("title REGEXP :text OR description REGEXP :text OR keywords REGEXP :text OR transcript REGEXP :text", text: words.join('|'))
 		end
-		scope
+		# end
+		# scope
 	end
 end
+
+# words = sampleString.downcase.strip.gsub(stopWords, '').split.uniq
