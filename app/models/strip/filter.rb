7956819cfd3543fb3bb2372ca424c44abaa9cc96
Samuel1698 <samuel1698500@gmail.com>
2021-07-01 12:56:31 -0400

    Restore query_params[:text}.present? if statement to filter as it gets called when rendering strip_index

--- app/models/strip/filter.rb
+++ app/models/strip/filter.rb
@@ -1,17 +1,17 @@
 class Strip::Filter
 	def filter(scope, query_params)
-		# if query_params[:text].present?
-		list = File.read("#{::Rails.root}/app/models/strip/stop-words.txt").split.join('|')
-		stopWords = /\b(?:#{list})\b/i
-		words = query_params[:text].downcase.strip.gsub(stopWords, '').split.uniq
-		# If the array is empty after excluding all the stop words
-		if !words.any?
-			scope = scope.where("title LIKE ?", ' ') # Returnn nothing. Must be a better way than doing a query
-		else
-			scope = scope.where("title REGEXP :text OR description REGEXP :text OR keywords REGEXP :text OR transcript REGEXP :text", text: words.join('|'))
+		if query_params[:text].present?
+			list = File.read("#{::Rails.root}/app/models/strip/stop-words.txt").split.join('|')
+			stopWords = /\b(?:#{list})\b/i
+			words = query_params[:text].downcase.strip.gsub(stopWords, '').split.uniq
+			# If the array is empty after excluding all the stop words
+			if !words.any?
+				scope = scope.where("title LIKE ?", ' ') # Returnn nothing. Must be a better way than doing a query
+			else
+				scope = scope.where("title REGEXP :text OR description REGEXP :text OR keywords REGEXP :text OR transcript REGEXP :text", text: words.join('|'))
+			end
 		end
-		# end
-		# scope
+		scope
 	end
 end
 
