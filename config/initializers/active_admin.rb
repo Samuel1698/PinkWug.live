413271915169c53258d0df51cd95b4c21faecf18
Samuel1698 <samuel1698500@gmail.com>
2021-07-08 09:43:46 -0400

    Change default admin route to comics_admin to avoid conflict with shopify existing admin route

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -28,7 +28,7 @@ ActiveAdmin.setup do |config|
   # will be added to.
   #
   # eg:
-  #   config.default_namespace = :hello_world
+  config.default_namespace = :comics_admin
   #
   # This will create resources in the HelloWorld module and
   # will namespace routes to /hello_world/*
