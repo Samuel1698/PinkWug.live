0103899f4ac4488d57c2158a89d646a574d5ba9a
Samuel1698 <samuel1698500@gmail.com>
2021-07-09 17:55:30 -0400

    uncomment debug code in whitespace.js

--- app/assets/javascripts/whiteSpace.js
+++ app/assets/javascripts/whiteSpace.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', addWhiteSpace("DOMContentLoaded"))
 function addWhiteSpace(content) {
   return function(){
     // Uncomment console.logs for debugging
-    console.log("---------------------------------");
-    console.log(content);
+    // console.log("---------------------------------");
+    // console.log(content);
     var viewPort      = window.innerHeight;
     var headerHeight  = document.querySelector(".header").offsetHeight;
     var contentHeight = document.querySelector("main").offsetHeight;
@@ -21,12 +21,12 @@ function addWhiteSpace(content) {
       document.querySelector(".white_space").style.minHeight = whiteSpace;
       // Makes a loop by calling itself as long as there is a difference
       setTimeout(addWhiteSpace("Time Out"), 200);
-      console.log("Success " + whiteSpace);
-      console.log("=================================")
+      // console.log("Success " + whiteSpace);
+      // console.log("=================================")
     }
-    else {
-      console.log("No change. \nwhiteSpace: " + whiteSpace + "\nDifference: " + difference);
-      console.log("=================================")
-    }
+    // else {
+    //   console.log("No change. \nwhiteSpace: " + whiteSpace + "\nDifference: " + difference);
+    //   console.log("=================================")
+    // }
   } 
 }
