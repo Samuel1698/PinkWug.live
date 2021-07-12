2a58c835b5650a1e275d7278ff8d43a5fa3e3304
Samuel1698 <samuel1698500@gmail.com>
2021-07-12 00:38:43 -0400

    dry comicNav.js after removal of one of the navigation bars

--- app/assets/javascripts/comicNav.js
+++ app/assets/javascripts/comicNav.js
@@ -1,36 +1,35 @@
 var current_strip  = window.location.pathname; 
-var first_strip    = document.querySelectorAll(".fa-angle-double-left");
-var previous_strip = document.querySelectorAll(".fa-angle-left");
-var next_strip     = document.querySelectorAll(".fa-angle-right");
-var last_strip     = document.querySelectorAll(".fa-angle-double-right");
+var first_strip    = document.querySelector(".fa-angle-double-left");
+var previous_strip = document.querySelector(".fa-angle-left");
+var next_strip     = document.querySelector(".fa-angle-right");
+var last_strip     = document.querySelector(".fa-angle-double-right");
 var backwardsDiv   = document.querySelector(".backwards").parentElement;
 var forwardDiv     = document.querySelector(".forward").parentElement;
 
-for (i = 0; i < 2; i++){
-  if (first_strip[i].pathname == current_strip) {
-    first_strip[i].querySelector(".visually-hidden").remove();
-    first_strip[i].removeAttribute('href');
-    first_strip[i].classList.add("disabled");
-  }
-  if (previous_strip[i].pathname == current_strip) {
-    previous_strip[i].querySelector(".visually-hidden").remove();
-    previous_strip[i].removeAttribute('href');
-    previous_strip[i].classList.add("disabled");
-    backwardsDiv.removeAttribute('href');
-    backwardsDiv.classList.add("disabled");
-  }
-  if (next_strip[i].pathname == current_strip || location.pathname == "/") {
-    next_strip[i].querySelector(".visually-hidden").remove();
-    next_strip[i].removeAttribute('href');
-    next_strip[i].classList.add("disabled");
-    forwardDiv.removeAttribute('href');
-    forwardDiv.classList.add("disabled");
-  }
-  if (last_strip[i].pathname == current_strip || location.pathname == "/") {
-    last_strip[i].querySelector(".visually-hidden").remove();
-    last_strip[i].removeAttribute('href');
-    last_strip[i].classList.add("disabled");
-  }
+
+if (first_strip.pathname == current_strip) {
+  first_strip.querySelector(".visually-hidden").remove();
+  first_strip.removeAttribute('href');
+  first_strip.classList.add("disabled");
+}
+if (previous_strip.pathname == current_strip) {
+  previous_strip.querySelector(".visually-hidden").remove();
+  previous_strip.removeAttribute('href');
+  previous_strip.classList.add("disabled");
+  backwardsDiv.removeAttribute('href');
+  backwardsDiv.classList.add("disabled");
+}
+if (next_strip.pathname == current_strip || location.pathname == "/") {
+  next_strip.querySelector(".visually-hidden").remove();
+  next_strip.removeAttribute('href');
+  next_strip.classList.add("disabled");
+  forwardDiv.removeAttribute('href');
+  forwardDiv.classList.add("disabled");
+}
+if (last_strip.pathname == current_strip || location.pathname == "/") {
+  last_strip.querySelector(".visually-hidden").remove();
+  last_strip.removeAttribute('href');
+  last_strip.classList.add("disabled");
 }
 
 var tooltip = document.querySelector('.tooltip');
