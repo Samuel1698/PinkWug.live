c44221ec3d4c34071d516bd1adf3b990607e0d87
Samuel1698 <samuel1698500@gmail.com>
2021-07-12 00:01:59 -0400

    Add javascript to admin dashboard to listen to load event from graphs. Use same font as newrelic to put 'ms' delay on FIP

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,44 +4,37 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc { I18n.t("active_admin.dashboard") } do
     columns do 
       div "Loading Graphs", class: "loading"
-      column do
-        # Memory Usage
-        # text_node %{<iframe src="https://chart-embed.service.newrelic.com/herald/3aa18aba-87c3-45dc-ad6b-5584a2ecad29?height=300px&timepicker=true" width="330" height="330" minwidth="330" scrolling="no" frameborder="no"></iframe>}.html_safe 
-      end
-      column do
-        # Threads
-        text_node %{<iframe src="https://chart-embed.service.newrelic.com/herald/4cdf8f76-c88a-4352-a4cf-fa635d04fed1?height=300px&timepicker=true" width="330" height="330" minwidth="330" scrolling="no" frameborder="no"></iframe>}.html_safe
-      end
-      column class: "column FID" do 
-        # First Input Delay
-        text_node %{<iframe src="https://chart-embed.service.newrelic.com/herald/302267e8-0d10-4427-96cb-62b4bce2d559?height=300px&timepicker=true" width="330" height="330" minwidth="330" scrolling="no" frameborder="no"></iframe>}.html_safe
+      column id: "FID", class: "column" do 
         div class: "figcaption disabled" do
           div class: "good" do   
-            span "100&nbsp;ms".html_safe
+            span "<100ms".html_safe
             span "Good"
           end
           div class: "average" do 
-            span "100&nbsp;ms&nbsp;-&nbsp;300&nbsp;ms".html_safe
+            span "100ms&nbsp;-&nbsp;300ms".html_safe
             span "Needs&nbsp;Work".html_safe
           end
           div class: "bad" do
-            span "400+&nbsp;ms".html_safe
+            span ">400ms".html_safe
             span "Bad"
           end
         end
+        div "ms", class: "ms disabled"
+      end
+      column id: "memory" do
+      end
+      column id: "threads" do
       end
     end
     columns do 
       div "Loading Graphs", class: "loading"
-      column do 
-        text_node %{<iframe src="https://chart-embed.service.newrelic.com/herald/ffa81e86-5425-4f3b-83d5-259ea15cd8bb?height=400px&timepicker=true" width="990" height="430" scrolling="no" frameborder="no"></iframe>}.html_safe
+      column id: "graph" do
       end
     end
     columns do
       column do 
         panel "Recent Comics" do 
           table_for Strip.all.order("created_at desc").limit(3) do 
-            column("Comic") { |strip| image_tag url_for(strip.image.variant(resize_to_limit:[200,10000])) }
             column("Title") { |strip| strip.title }
             column("Created in") { |strip| strip.created_at }
           end
--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -1,15 +1,57 @@
-var iframe = document.querySelector(".column").createElement("iframe");
-iframe.src = "https://chart-embed.service.newrelic.com/herald/3aa18aba-87c3-45dc-ad6b-5584a2ecad29?height=300px&timepicker=true";
+document.addEventListener("DOMContentLoaded", function(){
+	var iframeFID = document.createElement("iframe");
+	iframeFID.src = "https://chart-embed.service.newrelic.com/herald/302267e8-0d10-4427-96cb-62b4bce2d559?height=300px&timepicker=true";
+	iframeFID.style.width = '330px';
+	iframeFID.style.minWidth = '330px';
+	iframeFID.style.height = '330px';
+	iframeFID.scrolling = 'no';
+	iframeFID.style.border = 'none';
 
-if (iframe.addEventListener){
-    iframe.addEventListener("load", function(){
-        console.log("Local iframe is now loaded.");
-    });
-} 
-else {
-    iframe.load = function(){
-        console.log("Local iframe is now loaded.");
-    };
-}
+	var iframeMemory = document.createElement("iframe");
+	iframeMemory.src = "https://chart-embed.service.newrelic.com/herald/3aa18aba-87c3-45dc-ad6b-5584a2ecad29?height=300px&timepicker=true";
+	iframeMemory.style.width = '330px';
+	iframeMemory.style.minWidth = '330px';
+	iframeMemory.style.height = '330px';
+	iframeMemory.scrolling = 'no';
+	iframeMemory.style.border = 'none';
 
-document.body.appendChild(iframe);
+	var iframeThreads = document.createElement("iframe");
+	iframeThreads.src = "https://chart-embed.service.newrelic.com/herald/4cdf8f76-c88a-4352-a4cf-fa635d04fed1?height=300px&timepicker=true";
+	iframeThreads.style.width = '330px';
+	iframeThreads.style.minWidth = '330px';
+	iframeThreads.style.height = '330px';
+	iframeThreads.scrolling = 'no';
+	iframeThreads.style.border = 'none';
+
+
+	var iframeGraph = document.createElement("iframe");
+	iframeGraph.src = "https://chart-embed.service.newrelic.com/herald/ffa81e86-5425-4f3b-83d5-259ea15cd8bb?height=400px&timepicker=true";
+	iframeGraph.style.width = '990px';
+	iframeGraph.style.minWidth = '990px';
+	iframeGraph.style.height = '430px';
+	iframeGraph.scrolling = 'no';
+	iframeGraph.style.border = 'none';
+
+	var loading = document.querySelectorAll(".loading");
+
+	function disableElements(){
+		setTimeout(function(){
+			for (let i=0; i < loading.length;i++){
+				loading[i].classList.add("disabled");
+			}
+			document.querySelector(".figcaption").classList.remove("disabled");
+			document.querySelector(".ms").classList.remove("disabled");
+		}, 100);
+	}
+
+	if (iframeFID.addEventListener){
+		iframeFID.addEventListener("load", disableElements());
+	} 
+	else {
+	  iframeFID.load = disableElements();
+	}
+	document.getElementById("memory").appendChild(iframeMemory);
+	document.getElementById("threads").appendChild(iframeThreads);
+	document.getElementById("FID").appendChild(iframeFID);
+	document.getElementById("graph").appendChild(iframeGraph);
+});
