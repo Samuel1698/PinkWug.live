139d8309328a97e5b48d421c5c07bfba1d731900
Samuel1698 <samuel1698500@gmail.com>
2021-07-13 18:19:14 -0400

    Use vcr to create cassette of http requests. Remove httparty as it is not used. Re-write store specs

--- spec/features/user_visits_store_spec.rb
+++ spec/features/user_visits_store_spec.rb
@@ -4,73 +4,124 @@ Capybara.javascript_driver = :poltergeist
 
 RSpec.feature "user visits store", type: :feature, js: true do
   before do
+    VCR.use_cassette('shopify') do 
+      uri = URI.parse("https://#{ENV['SHOPIFY_API_LINK']}/admin/api/2021-07/graphql.json")
+      request = Net::HTTP::Post.new(uri)
+      request.content_type = "application/graphql"
+      request["X-Shopify-Access-Token"] = "#{ENV['SHOPIFY_API_SECRET']}"
+      request.body = "
+        {
+          collections(first: 10) {
+            edges {
+              node {
+                title
+                id
+                products(first: 12) {
+                  edges {
+                    node {
+                      id
+                      title
+                      handle
+                      featuredImage {
+                        originalSrc
+                      }
+                      variants(first: 1) {
+                        edges {
+                          node {
+                            price
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        "
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+      JSON.parse(response.body)
+    end
     # User visits store page
     visit store_index_path
   end
-  it "should display all products by collection" do
-    # Check that at least one Product exists under Mugs
-    within find('.collection.mugs', match: :first) do
+
+  it "should display all products by collection", :vcr do
+    # Get all collections
+    collections = page.all(".collection")
+
+    # Check that the correct Product exists under Mugs
+    within collections[0] do 
       within find('.product', match: :first) do 
-        expect(page).to have_text("fuck capitalism")
+        expect(page).to have_text("trans liberation now")
       end
     end
-    # Check that at least one Product exists under Clothes
-    within find('.collection.clothes', match: :first) do
+    # Check that the correct Product exists under Clothes
+    within collections[1] do 
       within find('.product', match: :first) do 
         expect(page).to have_text("the pinkest hoodie")
       end
     end
-    # Check that at least one Product exists under Stickers
-    within find('.collection.stickers', match: :first) do
-      within find('.product', match: :first) do 
-        expect(page).to have_text("the baron")
-      end
-    end
-    # Check that at least one Product exists under Prints
-    within find('.collection.prints', match: :first) do
+    # Check that the correct Product exists under Prints
+    within collections[2] do 
       within find('.product', match: :first) do 
         expect(page).to have_text("general strike")
       end
     end
+    # Check that the correct Product exists under Stickers
+    within collections[3] do 
+      within find('.product', match: :first) do 
+        expect(page).to have_text("the baron")
+      end
+    end
   end
-  it "should collapse collections on click" do
+  it "should collapse collections on click", :vcr do
+    # Get all buttons
+    buttons = page.all(".collection_button")
+
     # Find Mugs button
-    find_by_id('mugs').click
+    buttons[0].click
     # Expect the collection element to not be present
-    expect(page).to_not have_css('.collection.mugs')
+    expect(page).to_not have_text('trans liberation now')
     # Click button again
-    find_by_id('mugs').click
+    buttons[0].click
     # Expect the collection element to be present
-    expect(page).to have_css('.collection.mugs')
+    expect(page).to have_text('trans liberation now')
 
 
     # Find Clothes button
-    find_by_id('clothes').click
+    buttons[1].click
     # Expect the collection element to not be present
-    expect(page).to_not have_css('.collection.clothes')
+    expect(page).to_not have_text('the pinkest hoodie')
     # Click button again
-    find_by_id('clothes').click
+    buttons[1].click
     # Expect the collection element to be present
-    expect(page).to have_css('.collection.clothes')
+    expect(page).to have_text('the pinkest hoodie')
 
 
-    # Find Stickers button
-    find_by_id('stickers').click
+    # Find Prints button
+    buttons[2].click
     # Expect the collection element to not be present
-    expect(page).to_not have_css('.collection.stickers')
+    expect(page).to_not have_text('general strike')
     # Click button again
-    find_by_id('stickers').click
+    buttons[2].click
     # Expect the collection element to be present
-    expect(page).to have_css('.collection.stickers')
+    expect(page).to have_text('general strike')
 
     
-    # Find Prints button
-    find_by_id('prints').click
+    # Find Sticker button
+    buttons[3].click
     # Expect the collection element to not be present
-    expect(page).to_not have_css('.collection.prints')
+    expect(page).to_not have_text('the baron')
     # Click button again
-    find_by_id('prints').click
+    buttons[3].click
     # Expect the collection element to be present
-    expect(page).to have_css('.collection.prints')
+    expect(page).to have_text('the baron')
   end
 end
