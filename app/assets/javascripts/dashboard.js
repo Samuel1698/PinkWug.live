5c4f7083eee2f86f506f246c63b17135b898c82b
Samuel1698 <samuel1698500@gmail.com>
2021-07-13 03:38:09 -0400

    Improve graph functionality, size, and style in admin dashboard. Rename graph.js to dashboard.js

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,25 +1,28 @@
 document.addEventListener("DOMContentLoaded", function(){
 	var FID = document.createElement("iframe");
-	FID.src = "https://chart-embed.service.newrelic.com/herald/9879c337-03f7-49d6-9085-cad9431d31cd?height=300px&timepicker=true";
-	FID.style.width = '330px';
-	FID.style.minWidth = '330px';
-	FID.style.height = '330px';
+	FID.src = "https://chart-embed.service.newrelic.com/herald/a3f53eff-e6a7-40e2-9be5-6760347a9a3d?height=300px&timepicker=true";
+	FID.style.width = '300px';
+	FID.style.height = '350px';
+	FID.style.zIndex = '0';
+	FID.style.position = 'relative';
 	FID.scrolling = 'no';
 	FID.style.border = 'none';
 
 	var CLS = document.createElement("iframe");
-	CLS.src = "https://chart-embed.service.newrelic.com/herald/5c054812-6512-4a62-8e34-acfd694f4d3a?height=300px&timepicker=true";
-	CLS.style.width = '330px';
-	CLS.style.minWidth = '330px';
-	CLS.style.height = '330px';
+	CLS.src = "https://chart-embed.service.newrelic.com/herald/a7d5c912-36ca-4021-9b4e-c903c995a493?height=300px&timepicker=true";
+	CLS.style.width = '300px';
+	CLS.style.height = '350px';
+	CLS.style.zIndex = '0';
+	CLS.style.position = 'relative';
 	CLS.scrolling = 'no';
 	CLS.style.border = 'none';
 
 	var LCP = document.createElement("iframe");
-	LCP.src = "https://chart-embed.service.newrelic.com/herald/93dcf229-8057-4226-a7c7-6bfa96159fe1?height=300px&timepicker=true";
-	LCP.style.width = '100%';
-	LCP.style.minWidth = '330px';
-	LCP.style.height = '330px';
+	LCP.src = "https://chart-embed.service.newrelic.com/herald/f8389095-dd0c-435b-a80d-540645b96641?height=300px&timepicker=true";
+	LCP.style.width = '300px';
+	LCP.style.height = '350px';
+	LCP.style.zIndex = '0';
+	LCP.style.position = 'relative';
 	LCP.scrolling = 'no';
 	LCP.style.border = 'none';
 
@@ -27,13 +30,17 @@ document.addEventListener("DOMContentLoaded", function(){
 	var Graph = document.createElement("iframe");
 	Graph.src = "https://chart-embed.service.newrelic.com/herald/531ac35c-9a01-4f14-8476-34293f2fdce7?height=400px&timepicker=true";
 	Graph.style.width = '100%';
-	Graph.style.minWidth = '990px';
+	Graph.style.minWidth = '900px';
 	Graph.style.height = '430px';
+	Graph.style.zIndex = '0';
+	Graph.style.position = 'relative';
 	Graph.scrolling = 'no';
 	Graph.style.border = 'none';
 
 	var IDs = ["FID", "CLS", "LCP", "Graph"];
-
+	if (document.getElementById("page_title").innerText == "Dashboard"){
+		document.getElementById("main_content").classList.add("mine");
+	}
 	function disableElements(ID){
 		var loading = document.getElementById(ID).querySelector(".loading");
 		setTimeout(function(){
@@ -41,9 +48,6 @@ document.addEventListener("DOMContentLoaded", function(){
 			loading.parentElement.querySelector(".tooltip").classList.remove("disabled");
 			if (loading.parentElement.getElementsByTagName('div')[1].classList.contains("figcaption")){
 				loading.parentElement.querySelector(".figcaption").classList.remove("disabled");
-			}
-			if (ID == "FID" || ID == "LCP"){
-				loading.parentElement.querySelector(".unit").classList.remove("disabled");
 			}
 		}, 4000);
 	}
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -219,7 +219,7 @@ ActiveAdmin.setup do |config|
   #   config.register_stylesheet 'my_print_stylesheet.css', media: :print
   #
   # To load a javascript file:
-  config.register_javascript 'graphs.js'
+  config.register_javascript 'dashboard.js'
 
   # == CSV options
   #
