6afcd8aab78b1746d075bb8e711862ef1b78ea1b
Samuel1698 <samuel1698500@gmail.com>
2021-07-16 15:44:34 -0400

    Modify URI in store spec

--- spec/features/user_visits_store_spec.rb
+++ spec/features/user_visits_store_spec.rb
@@ -5,28 +5,33 @@ RSpec.feature "user visits store", type: :feature, js: true do
   before do
     VCR.use_cassette('shopify') do 
       uri = URI.parse("https://pinkwug-dev-store.myshopify.com/admin/api/2021-07/graphql.json")
-      request = Net::HTTP::Post.new(uri)
-      request.content_type = "application/graphql"
-      request.body = "
-        {
-          collections(first: 10) {
-            edges {
-              node {
-                title
-                id
-                products(first: 12) {
-                  edges {
-                    node {
-                      id
-                      title
-                      handle
-                      featuredImage {
-                        originalSrc
-                      }
-                      variants(first: 1) {
-                        edges {
-                          node {
-                            price
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+      Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        request = Net::HTTP::Post.new uri
+        request.content_type = "application/graphql"
+        request.body = "
+          {
+            collections(first: 10) {
+              edges {
+                node {
+                  title
+                  id
+                  products(first: 12) {
+                    edges {
+                      node {
+                        id
+                        title
+                        handle
+                        featuredImage {
+                          originalSrc
+                        }
+                        variants(first: 1) {
+                          edges {
+                            node {
+                              price
+                            }
                           }
                         }
                       }
@@ -36,15 +41,13 @@ RSpec.feature "user visits store", type: :feature, js: true do
               }
             }
           }
-        }
-        "
-      req_options = {
-        use_ssl: uri.scheme == "https",
-      }
-      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-        http.request(request)
+          "
+        response = http.request(request)
+        JSON.parse(response.body)
       end
-      JSON.parse(response.body)
+      # response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      #   http.request(request)
+      # end
     end
     # User visits store page
     visit store_index_path
