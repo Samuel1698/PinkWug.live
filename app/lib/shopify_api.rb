6c339ab5cbece0a20857b09ea5734a1b09fe5144
Samuel1698 <samuel1698500@gmail.com>
2021-07-16 15:50:58 -0400

    Modify shopify_api to match the store spec

--- app/lib/shopify_api.rb
+++ app/lib/shopify_api.rb
@@ -2,47 +2,89 @@ class ShopifyApi
 	require 'net/http'
 	require 'uri'
 	def response 
-		uri = URI.parse("https://#{ENV['SHOPIFY_API_LINK']}/admin/api/2021-07/graphql.json")
-		request = Net::HTTP::Post.new(uri)
-		request.content_type = "application/graphql"
-		request["X-Shopify-Access-Token"] = "#{ENV['SHOPIFY_API_SECRET']}"
-		request.body = "
-		  {
-			  collections(first: 10) {
-			    edges {
-			      node {
-			      	title
-			        id
-			        products(first: 12) {
-			          edges {
-			            node {
-			              id
-			              title
-			              handle
-			              featuredImage {
-			                originalSrc
-			              }
-			              variants(first: 1) {
-			                edges {
-			                  node {
-			                    price
-			                  }
-			                }
-			              }
-			            }
-			          }
-			        }
-			      }
-			    }
-			  }
-			}
-		  "
+		uri = URI.parse("https://pinkwug-dev-store.myshopify.com/admin/api/2021-07/graphql.json")
 		req_options = {
 		  use_ssl: uri.scheme == "https",
 		}
-		response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-		  http.request(request)
+		Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+		  request = Net::HTTP::Post.new uri
+		  request.content_type = "application/graphql"
+		  request["X-Shopify-Access-Token"] = "#{ENV['SHOPIFY_API_SECRET']}"
+		  request.body = "
+		    {
+		      collections(first: 10) {
+		        edges {
+		          node {
+		            title
+		            id
+		            products(first: 12) {
+		              edges {
+		                node {
+		                  id
+		                  title
+		                  handle
+		                  featuredImage {
+		                    originalSrc
+		                  }
+		                  variants(first: 1) {
+		                    edges {
+		                      node {
+		                        price
+		                      }
+		                    }
+		                  }
+		                }
+		              }
+		            }
+		          }
+		        }
+		      }
+		    }
+		    "
+		  response = http.request(request)
+		  JSON.parse(response.body)
 		end
-		JSON.parse(response.body)
+		# uri = URI.parse("https://#{ENV['SHOPIFY_API_LINK']}/admin/api/2021-07/graphql.json")
+		# request = Net::HTTP::Post.new(uri)
+		# request.content_type = "application/graphql"
+		# request["X-Shopify-Access-Token"] = "#{ENV['SHOPIFY_API_SECRET']}"
+		# request.body = "
+		#   {
+		# 	  collections(first: 10) {
+		# 	    edges {
+		# 	      node {
+		# 	      	title
+		# 	        id
+		# 	        products(first: 12) {
+		# 	          edges {
+		# 	            node {
+		# 	              id
+		# 	              title
+		# 	              handle
+		# 	              featuredImage {
+		# 	                originalSrc
+		# 	              }
+		# 	              variants(first: 1) {
+		# 	                edges {
+		# 	                  node {
+		# 	                    price
+		# 	                  }
+		# 	                }
+		# 	              }
+		# 	            }
+		# 	          }
+		# 	        }
+		# 	      }
+		# 	    }
+		# 	  }
+		# 	}
+		#   "
+		# req_options = {
+		#   use_ssl: uri.scheme == "https",
+		# }
+		# response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+		#   http.request(request)
+		# end
+		# JSON.parse(response.body)
 	end
 end
