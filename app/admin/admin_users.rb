e74e704013c64d071b992333684123ac5b9894a9
Samuel1698 <samuel1698500@gmail.com>
2021-07-19 11:36:55 -0400

    Edit admin_users show page. Modify specs to fit new format in admin crud. Add mailer url for all environments

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -1,21 +1,30 @@
 ActiveAdmin.register AdminUser do
   permit_params :email, :password, :password_confirmation
 
+  filter :email
+  filter :created_at
+
+  ActiveAdmin.setup do |config|
+    config.localize_format = :long
+  end
+
   index do
     selectable_column
     id_column
     column :email
-    column :current_sign_in_at
-    column :sign_in_count
     column :created_at
     actions
   end
 
-  filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
-
+  show do 
+    attributes_table do 
+      row :email 
+      row :reset_password_token
+      row :reset_password_sent_at 
+      row :created_at
+      row :updated_at  
+    end
+  end
   
   form do |f|
     f.inputs do
