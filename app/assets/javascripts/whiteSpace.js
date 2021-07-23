a70517d177721098f0dfb6ecf6f0ca6b43c9b88b
Samuel1698 <samuel1698500@gmail.com>
2021-07-23 10:46:03 -0400

    Add shop navigation icons & shelf. Add javascript to detect if a product is placed in the wrong collection and put it where it should be

--- app/assets/javascripts/whiteSpace.js
+++ app/assets/javascripts/whiteSpace.js
@@ -1,7 +1,7 @@
 //Event for window resize
-window.addEventListener('resize', addWhiteSpace("resize")); 
+window.addEventListener('resize', addWhiteSpace("resize", 20)); 
 //Turbolinks navigation
-document.addEventListener('DOMContentLoaded', addWhiteSpace("DOMContentLoaded")); 
+document.addEventListener('DOMContentLoaded', addWhiteSpace("DOMContentLoaded", 15)); 
 
 function addWhiteSpace(content) {
   return function(){
@@ -14,14 +14,14 @@ function addWhiteSpace(content) {
     var footerHeight  = document.querySelector(".footer").offsetHeight;
 
     var whiteSpace    = document.querySelector(".white_space").offsetHeight;
-    var difference    = viewPort - (headerHeight + contentHeight + footerHeight + 25);
+    var difference    = viewPort - (headerHeight + contentHeight + footerHeight);
     difference = difference > 0 ? difference: 0;
     if (whiteSpace != difference){
-      whiteSpace = difference + "px";
-      document.querySelector(".white_space").style.minHeight = whiteSpace;
+      let space = difference + "px";
+      document.querySelector(".white_space").style.minHeight = space;
       // Makes a loop by calling itself as long as there is a difference
-      setTimeout(addWhiteSpace("Time Out"), 200);
-      // console.log("Success " + whiteSpace);
+      // setTimeout(addWhiteSpace("Loop"), 400);
+      // console.log("Success " + space);
       // console.log("=================================")
     }
     // else {
