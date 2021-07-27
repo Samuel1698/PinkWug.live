21d94c6ff70230db979ebc0db3a08f703ebfa8f8
Samuel1698 <samuel1698500@gmail.com>
2021-07-27 18:07:32 -0400

    Change emails to use pinkwug's. Use protect_from_forgery prepend on application controller

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,5 @@ class AdminUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
+
 end
