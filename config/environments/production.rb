c1c8a39385f35253391cecebfd14e0ac0e48f39c
Samuel1698 <samuel1698500@gmail.com>
2021-07-27 22:09:04 -0400

    add asset host to action mailer in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,8 @@ Rails.application.configure do
   config.eager_load = true
 
   # Use heroku/pinkwug.live as action mailer url
-  config.action_mailer.default_url_options = { :host => "https://pinkwug.live" }
+  config.action_mailer.default_url_options = { :host => "https://www.pinkwug.live" }
+  config.action_mailer.asset_host = 'https://www.pinkwug.live'
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
