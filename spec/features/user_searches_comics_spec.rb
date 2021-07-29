c9f5d000268862883576174bd5005886ee1dff27
Samuel1698 <samuel1698500@gmail.com>
2021-07-29 17:40:35 -0400

    Modify search spec to work with new layout

--- spec/features/user_searches_comics_spec.rb
+++ spec/features/user_searches_comics_spec.rb
@@ -1,42 +1,41 @@
 require 'rails_helper'
 Capybara.javascript_driver = :selenium_chrome_headless
 
-RSpec.feature "user searches comics", type: :feature, js: true
-  # before do 
-  #   # Visit home page
-  #   visit root_path
-  #   # Click on search
-  #   find('.search_toggle').click
-  #   # Make sure the search form is active
-  #   expect(page).to have_css('.search_form')
-  # end
-  # it "should display 3 union results on one word search" do 
-  #   # Fill in the search form (input) with 'union'
-  #   fill_in('query_text', with: 'Union')
-  #   # Click on submit button
-  #   find('.submit_button').click
-  #   # Check that there are 3 rows
-  #   within find('.table') do 
-  #     expect(page).to have_css('.row', count: 3)
-  #   end
-  # end
-  # it "should display 4 results on 'Union LTV' search" do 
-  #   # Fill in the search form with 'union LTV'
-  #   fill_in('query_text', with: 'Union LTV')
-  #   # Click on submit button
-  #   find('.submit_button').click
-  #   # Check that there are 4 rows
-  #   within find('.table') do 
-  #     expect(page).to have_css('.row', count: 4)
-  #   end
-  # end
-  # it "should display 4 results on 'Union and LTV search" do 
-  #   # Fill in the search form with 'union LTV'
-  #   fill_in('query_text', with: 'Union LTV')
-  #   # Click on submit button
-  #   find('.submit_button').click
-  #   # Check that there are 4 rows
-  #   within find('.table') do 
-  #     expect(page).to have_css('.row', count: 4)
-  #   end
-  # end
+RSpec.feature "user searches comics", type: :feature, js: true do
+  before do 
+    # Visit archive page
+    visit strips_path
+    # Make sure the search form is active
+    expect(page).to have_css('.search_form')
+  end
+  it "should display 3 union results on one word search" do 
+    # Fill in the search form (input) with 'union'
+    fill_in('query_text', with: 'Union')
+    # Click on submit button
+    find('.submit_button').click
+    # Check that there are 3 rows
+    within find('.table') do 
+      expect(page).to have_css('.row', count: 3)
+    end
+  end
+  it "should display 4 results on 'Union LTV' search" do 
+    # Fill in the search form with 'union LTV'
+    fill_in('query_text', with: 'Union LTV')
+    # Click on submit button
+    find('.submit_button').click
+    # Check that there are 4 rows
+    within find('.table') do 
+      expect(page).to have_css('.row', count: 4)
+    end
+  end
+  it "should display 4 results on 'Union and LTV search" do 
+    # Fill in the search form with 'union LTV'
+    fill_in('query_text', with: 'Union LTV')
+    # Click on submit button
+    find('.submit_button').click
+    # Check that there are 4 rows
+    within find('.table') do 
+      expect(page).to have_css('.row', count: 4)
+    end
+  end
+end
