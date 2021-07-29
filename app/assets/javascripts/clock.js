7a561faa446387b1ba356f26b2fc2f327299e043
Samuel1698 <samuel1698500@gmail.com>
2021-07-29 00:28:22 -0400

    Use provided images for clock hands

--- app/assets/javascripts/clock.js
+++ app/assets/javascripts/clock.js
@@ -1,25 +1,21 @@
+let h_hand = document.querySelector(".hour");
+let m_hand = document.querySelector(".minute");
+
 function startTime(){
+  setClock();
+  setInterval(function() {
+    setClock();
+  }, 30000);
+}
+
+function setClock(){
   let today = new Date();
-  let h_hand = document.querySelector(".hour-hand");
-  let m_hand = document.querySelector(".minu-hand");
   let h = today.getHours();
   let m = today.getMinutes();
 
-  h_degrees = h * 15;
-  m_degrees = m * 6;
+  h_degrees = h * 15; //360 % 24
+  m_degrees = m * 6;  //360 % 60 
 
   h_hand.style.transform = `rotate(${h_degrees}deg)`;
   m_hand.style.transform = `rotate(${m_degrees}deg)`;
-  setInterval(function() {
-    let today = new Date();
-
-    let h = today.getHours();
-    let m = today.getMinutes();
-
-    h_degrees = h * 15;
-    m_degrees = m * 6;
-
-    h_hand.style.transform = `rotate(${h_degrees}deg)`;
-    m_hand.style.transform = `rotate(${m_degrees}deg)`;
-  }, 30000);
 }
