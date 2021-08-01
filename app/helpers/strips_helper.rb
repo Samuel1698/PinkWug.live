10a92b355e911ba3074f0b1206ccec7ce238e5dd
samuel1698 <samuel1698500@gmail.com>
2021-08-01 14:08:18 -0400

    Use webp format for image variants for better compression

--- app/helpers/strips_helper.rb
+++ app/helpers/strips_helper.rb
@@ -1,6 +1,6 @@
 module StripsHelper
   def strip_variant(s, resolution)
-    url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [85]).processed)
+    url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [85],format: :webp).processed)
   end
   def split_url(url)
     URI(url).host.split('.')[-2]
