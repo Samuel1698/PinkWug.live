ad9e84a367409a55fcd001ba5f8d4044853f9374
Samuel1698 <samuel1698500@gmail.com>
2021-08-04 13:37:32 -0400

    Set quality to 100 for non-webp images

--- app/helpers/strips_helper.rb
+++ app/helpers/strips_helper.rb
@@ -3,7 +3,7 @@ module StripsHelper
     if request.env['HTTP_ACCEPT'].include?('image/webp')
       url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp).processed)
     else 
-      url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [85]).processed)
+      url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [100]).processed)
     end
   end
   def split_url(url)
