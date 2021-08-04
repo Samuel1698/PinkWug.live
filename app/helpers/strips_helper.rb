cc0405dd67a6d4a9cee6f79746a0e2290bf55a20
Samuel1698 <samuel1698500@gmail.com>
2021-08-04 19:32:27 -0400

    Use representation_url instead to attempt to avoid active storage redirect

--- app/helpers/strips_helper.rb
+++ app/helpers/strips_helper.rb
@@ -1,11 +1,9 @@
 module StripsHelper
   def strip_variant(s, resolution)
     if request.env['HTTP_ACCEPT'] && request.env['HTTP_ACCEPT'].include?('image/webp') 
-      url = url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp).processed)
-      url.sub(/\?.*/, '')
+      Rails.application.routes.url_helpers.rails_representation_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp).processed, only_path: true)
     else 
-      url = url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [100]).processed)
-      url.sub(/\?.*/, '')
+      Rails.application.routes.url_helpers.rails_representation_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [100]).processed, only_path: true)
     end
   end
   def split_url(url)
