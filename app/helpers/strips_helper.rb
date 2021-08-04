3dcd5b0434bfbdc9d5897bd9c2b81a85a394de97
Samuel1698 <samuel1698500@gmail.com>
2021-08-04 13:36:08 -0400

    Check if browser supports webp before rendering the comic variants

--- app/helpers/strips_helper.rb
+++ app/helpers/strips_helper.rb
@@ -1,6 +1,10 @@
 module StripsHelper
   def strip_variant(s, resolution)
-    url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [85],format: :webp).processed)
+    if request.env['HTTP_ACCEPT'].include?('image/webp')
+      url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp).processed)
+    else 
+      url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [85]).processed)
+    end
   end
   def split_url(url)
     URI(url).host.split('.')[-2]
