af656fb54b450cdb08170afbd7e5010d444c5635
Samuel1698 <samuel1698500@gmail.com>
2021-08-04 15:09:22 -0400

    Make condition test for presence of request.env['HTTP_ACCEPT'] as well

--- app/helpers/strips_helper.rb
+++ app/helpers/strips_helper.rb
@@ -1,10 +1,10 @@
 module StripsHelper
   def strip_variant(s, resolution)
-    if request.env['HTTP_ACCEPT'].include?('image/webp')
-      url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp).processed)
-    else 
-      url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [100]).processed)
-    end
+      if request.env['HTTP_ACCEPT'] && request.env['HTTP_ACCEPT'].include?('image/webp') 
+        url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp).processed)
+      else 
+        url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [100]).processed)
+      end
   end
   def split_url(url)
     URI(url).host.split('.')[-2]
