32e75e06ea5a2d5c2ffe74fbc6abbc7b87fb3c30
Samuel1698 <samuel1698500@gmail.com>
2021-08-04 23:54:51 -0400

    Use different url helper for production environment

--- app/helpers/strips_helper.rb
+++ app/helpers/strips_helper.rb
@@ -1,9 +1,13 @@
 module StripsHelper
   def strip_variant(s, resolution)
-    if request.env['HTTP_ACCEPT'] && request.env['HTTP_ACCEPT'].include?('image/webp') 
+    if Rails.env.production?
+      if request.env['HTTP_ACCEPT'] && request.env['HTTP_ACCEPT'].include?('image/webp') 
+        rails_public_blob_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp))
+      else 
+        rails_public_blob_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [100]))
+      end
+    else
       rails_representation_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp))
-    else 
-      rails_representation_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [100]))
     end
   end
   def split_url(url)
