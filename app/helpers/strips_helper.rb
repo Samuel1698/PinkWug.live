5525a5341941ef35c016f1316faf87b4a64bd54d
Samuel1698 <samuel1698500@gmail.com>
2021-08-04 23:17:31 -0400

    Make default image uploads public. Use custom s3 public website routed through cloudflare to host the images and cache them for 1 year

--- app/helpers/strips_helper.rb
+++ app/helpers/strips_helper.rb
@@ -1,9 +1,13 @@
 module StripsHelper
   def strip_variant(s, resolution)
-    if request.env['HTTP_ACCEPT'] && request.env['HTTP_ACCEPT'].include?('image/webp') 
-      Rails.application.routes.url_helpers.rails_representation_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp).processed, only_path: true)
-    else 
-      Rails.application.routes.url_helpers.rails_representation_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [100]).processed, only_path: true)
+    if Rails.env.production?
+      if request.env['HTTP_ACCEPT'] && request.env['HTTP_ACCEPT'].include?('image/webp') 
+        'https://storage.pinkwug.live/' + s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp).key
+      else 
+        'https://storage.pinkwug.live/' + s.image.variant(resize_to_limit:[resolution,nil], quality: [100]).key
+      end
+    else
+      url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp))
     end
   end
   def split_url(url)
