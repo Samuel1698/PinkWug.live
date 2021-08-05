ab13acb0835740fa84117bd1291a693ea73a48e8
Samuel1698 <samuel1698500@gmail.com>
2021-08-05 00:07:23 -0400

    try storage.yml of s3_public

--- app/helpers/strips_helper.rb
+++ app/helpers/strips_helper.rb
@@ -1,14 +1,16 @@
 module StripsHelper
   def strip_variant(s, resolution)
-    if Rails.env.production?
+    # if Rails.env.production?
       if request.env['HTTP_ACCEPT'] && request.env['HTTP_ACCEPT'].include?('image/webp') 
-        rails_public_blob_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp))
+        # rails_public_blob_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp))
+        url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp))
       else 
-        rails_public_blob_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [100]))
+        # rails_public_blob_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [100]))
+        url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [100]))
       end
-    else
-      rails_representation_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp))
-    end
+    # else
+    #   rails_representation_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp))
+    # end
   end
   def split_url(url)
     URI(url).host.split('.')[-2]
