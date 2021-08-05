f9c82b29ece716761d006a877cb57d064f8bf571
Samuel1698 <samuel1698500@gmail.com>
2021-08-05 00:33:13 -0400

    Use active storage provided code for putting a CDN in front of itself

--- app/helpers/strips_helper.rb
+++ app/helpers/strips_helper.rb
@@ -2,12 +2,12 @@ module StripsHelper
   def strip_variant(s, resolution)
     if Rails.env.production?
       if request.env['HTTP_ACCEPT'] && request.env['HTTP_ACCEPT'].include?('image/webp') 
-        "https://#{ENV['S3_BUCKET_NAME']}/" + s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp).key
+        cdn_image_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp))
       else 
-        "https://#{ENV['S3_BUCKET_NAME']}/" + s.image.variant(resize_to_limit:[resolution,nil], quality: [100]).key
+        cdn_image_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [100]))
       end
     else
-      rails_representation_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp))
+      url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp))
     end
   end
   def split_url(url)
--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,27 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  direct :cdn_image do |model, options|
+    if model.respond_to?(:signed_id)
+      route_for(
+        :rails_service_blob_proxy,
+        model.signed_id,
+        model.filename,
+        options.merge(host: ENV['S3_BUCKET_NAME'])
+      )
+    else
+      signed_blob_id = model.blob.signed_id
+      variation_key  = model.variation.key
+      filename       = model.blob.filename
+
+      route_for(
+        :rails_blob_representation_proxy,
+        signed_blob_id,
+        variation_key,
+        filename,
+        options.merge(host: ENV['S3_BUCKET_NAME'])
+      )
+    end
+  end
 end
