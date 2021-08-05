5ffae7d2f2ae12515f10757458555db31ec7d5b6
Samuel1698 <samuel1698500@gmail.com>
2021-08-04 23:44:25 -0400

    Use direct routes instead of explicit url and handle variants

--- app/helpers/strips_helper.rb
+++ app/helpers/strips_helper.rb
@@ -1,13 +1,9 @@
 module StripsHelper
   def strip_variant(s, resolution)
-    if Rails.env.production?
-      if request.env['HTTP_ACCEPT'] && request.env['HTTP_ACCEPT'].include?('image/webp') 
-        'https://storage.pinkwug.live/' + s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp).key
-      else 
-        'https://storage.pinkwug.live/' + s.image.variant(resize_to_limit:[resolution,nil], quality: [100]).key
-      end
-    else
-      url_for(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp))
+    if request.env['HTTP_ACCEPT'] && request.env['HTTP_ACCEPT'].include?('image/webp') 
+      rails_representation_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [85], format: :webp))
+    else 
+      rails_representation_url(s.image.variant(resize_to_limit:[resolution,nil], quality: [100]))
     end
   end
   def split_url(url)
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,36 @@
 Rails.application.routes.draw do
   constraints(host: /^www\./i) do
-      get '(*any)' => redirect { |params, request|
-        URI.parse(request.url).tap { |uri| uri.host.sub!(/^www\./i, '') }.to_s
-      }
-   end
-   root controller: :strips, action: :show, id: -1
-   get 'about' => 'strips#about'
-   resources :archive, as: :strips, controller: :strips, only: [:index]
-   resources :comics, as: :strips, controller: :strips, only: [:show]
-   resources :shop, as: :store, controller: :store, only: [:index]
-   match "/404", to: "errors#not_found", via: :all
-   match "/422", to: "errors#unacceptable", via: :all
-   match "/500", to: "errors#internal_server_error", via: :all
-   
-   get '/sitemap.xml', to: 'sitemaps#index', defaults: { format: 'xml'}
+    get '(*any)' => redirect { |params, request|
+      URI.parse(request.url).tap { |uri| uri.host.sub!(/^www\./i, '') }.to_s
+    }
+  end
+  root controller: :strips, action: :show, id: -1
 
-   devise_for :admin_users, ActiveAdmin::Devise.config
-   ActiveAdmin.routes(self)
+  direct :rails_public_blob do |blob|
+    if Rails.env.test? || Rails.env.development?
+      route = 
+        # ActiveStorage::VariantWithRecord was introduced in Rails 6.1
+        # Remove the second check if you're using an older version
+        if blob.is_a?(ActiveStorage::Variant) || blob.is_a?(ActiveStorage::VariantWithRecord)
+          :rails_representation
+        else
+         :rails_blob
+        end
+      route_for(route, blob)
+    else
+      File.join("#{ENV['S3_BUCKET_NAME']}", blob.key)
+    end
+  end
+  get 'about' => 'strips#about'
+  resources :archive, as: :strips, controller: :strips, only: [:index]
+  resources :comics, as: :strips, controller: :strips, only: [:show]
+  resources :shop, as: :store, controller: :store, only: [:index]
+  match "/404", to: "errors#not_found", via: :all
+  match "/422", to: "errors#unacceptable", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+
+  get '/sitemap.xml', to: 'sitemaps#index', defaults: { format: 'xml'}
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 end
